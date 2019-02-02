216261990f1b03660da1b0321308ad7439c6e7b2
Edgar Ruiz <edgararuiz@users.noreply.github.com>
2019-02-02 07:00:44 -0600

    Improves dplyr dep references

--- R/dbplot.R
+++ R/dbplot.R
@@ -1,6 +1,7 @@
 #' @import rlang
-#' @import dplyr
 #' @import ggplot2
 #' @importFrom purrr imap
+#' @importFrom dplyr mutate summarise group_by %>% count
+#' @importFrom dplyr ungroup collect rename select n 
 #' @keywords internal
 utils::globalVariables(c("."))
